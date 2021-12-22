631ddc9a0d6540e2f8bef11b0231f727a3ac488c
lostsnow <lostsnow@gmail.com>
2021-12-22 16:11:46 +0800

    add ldap injection

---
+++ flasksrc/demo/ldap/ldap.py
@@ -0,0 +1,36 @@
+import ldap3
+from flask import request
+from ldap3.utils.conv import escape_filter_chars
+
+
+def ldap_search():
+    username = request.args.get('username', '')
+    password = request.args.get('password', '')
+
+    ldap_srv = ldap3.Server("ldap://ldap.forumsys.com:389")
+    ldap_conn = ldap3.Connection(ldap_srv, user="cn=read-only-admin,dc=example,dc=com", password="password",
+                                 auto_bind=True)
+
+    search_filter = "(&(cn=%s)(userPassword=%s))" % (username, password)
+    exists = ldap_conn.search("dc=example,dc=com", search_filter)
+    if not exists:
+        return "403"
+    return "200"
+
+
+def ldap_safe_search():
+    username = request.args.get('username', '')
+    password = request.args.get('password', '')
+
+    ldap_srv = ldap3.Server("ldap://ldap.forumsys.com:389")
+    ldap_conn = ldap3.Connection(ldap_srv, user="cn=read-only-admin,dc=example,dc=com", password="password",
+                                 auto_bind=True)
+
+    username = escape_filter_chars(username)
+    password = escape_filter_chars(password)
+
+    search_filter = "(&(cn=%s)(userPassword=%s))" % (username, password)
+    exists = ldap_conn.search("dc=example,dc=com", search_filter)
+    if not exists:
+        return "403"
+    return "200"
